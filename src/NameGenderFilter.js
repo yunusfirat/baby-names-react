ef0d329bb9360602ee6ad6e49fdd5953df8ac0a0
yunusfirat <yunusfirat44@gmail.com>
2021-05-11 01:35:52 +0100

    active class added

--- src/NameGenderFilter.js
+++ src/NameGenderFilter.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { GrRestroomWomen } from "react-icons/gr";
 import { ImMan } from "react-icons/im";
 import { FaPeopleCarry } from "react-icons/fa";
 import Data from "./babyNamesData.json";
 const NameGenderFilter = ({ setData }) => {
-
+    const [allActive, setAllActive] = useState(true);
+    const [maleActive, setmaleActive] = useState(false);
+    const [femaleActive, setFemaleActive] = useState(false);
     const filterMale = Data.filter((baby) => baby.sex === "m");
     const filterFemale = Data.filter((baby) => baby.sex === "f");
 
 
 
     const allfemale = () => {
-      setData(filterFemale);
+        setData(filterFemale);
+        setFemaleActive(true);
+        setmaleActive(false);
+        setAllActive(false);
     };
 
     const allmale = () => {
-       setData(filterMale);
+        setData(filterMale);
+        setFemaleActive(false);
+        setmaleActive(true);
+        setAllActive(false);
     };
     const all = () => {
         setData(Data);
+        setFemaleActive(false);
+        setmaleActive(false);
+        setAllActive(true);
     };
     return (
         <>
-        <div className="gender-button">
-            <div><button className="icon"style= {{ backgroundColor:"##DDDDDD" }} onClick={all} ><FaPeopleCarry  /></button></div>
-            <div><button className="icon" style= {{ backgroundColor:"#FFC6E5" }} onClick={allfemale} ><GrRestroomWomen /></button> </div>
-            <div><button className="icon" style= {{ backgroundColor:"#A0CFEE" }} onClick={allmale}><ImMan/></button> </div>
-        </div>
+            <div className="gender-button">
+                <div><button className={allActive ? "active" : "icon"} style={{ backgroundColor: "##DDDDDD" }} onClick={all} ><FaPeopleCarry /></button></div>
+                <div><button className={femaleActive ? "active" : "icon"}  style={{ backgroundColor: "#FFC6E5" }} onClick={allfemale} ><GrRestroomWomen /></button> </div>
+                <div><button className={maleActive ? "active" : "icon"}  style={{ backgroundColor: "#A0CFEE" }} onClick={allmale}><ImMan /></button> </div>
+            </div>
         </>
     );
 };
